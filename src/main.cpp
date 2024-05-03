84a6c5619c430df2e0b3da7fab05330f2b9707af
byenio <marcel.bienko@gmail.com>
2024-05-03 20:34:56 +0200

    init playground

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,38 @@
 #include "Session/Player/Player.hpp"
 #include "Session/Session.hpp"
 
-// Game *gameInit(sf::RenderWindow &rWindow)
-// {
-//   Player *pPlayer = new Player("byenio", 13);
-//   Racetrack *pRacetrack = new Racetrack("monza");
-//   Session *pSession = new Session(pPlayer, pRacetrack);
+Game *gameInit(sf::RenderWindow &rWindow)
+{
+  Player *pPlayer = new Player("byenio", 13);
+  Racetrack *pRacetrack = new Racetrack("monza");
+  Session *pSession = new Session(pPlayer, pRacetrack);
 
-//   Lap *pLap1 = new Lap(pPlayer, pRacetrack, 53000);
-//   Lap *pLap2 = new Lap(pPlayer, pRacetrack, 53001);
-//   Lap *pLap3 = new Lap(pPlayer, pRacetrack, 52999);
+  Lap *pLap1 = new Lap(pPlayer, pRacetrack, 53000);
+  Lap *pLap2 = new Lap(pPlayer, pRacetrack, 53001);
+  Lap *pLap3 = new Lap(pPlayer, pRacetrack, 52999);
 
-//   pSession->pushLap(pLap1);
-//   pSession->pushLap(pLap2);
-//   pSession->pushLap(pLap3);
+  pSession->pushLap(pLap1);
+  pSession->pushLap(pLap2);
+  pSession->pushLap(pLap3);
 
-//   sf::CircleShape circle(100.f);
-//   circle.setFillColor(sf::Color::Red);
-//   circle.setPosition(600, 200);
+  sf::CircleShape circle(100.f);
+  circle.setFillColor(sf::Color::Red);
+  circle.setPosition(600, 200);
 
-//   rWindow.clear();
-//   rWindow.draw(circle);
-//   rWindow.display();
+  rWindow.clear();
+  rWindow.draw(circle);
+  rWindow.display();
 
-//   Game *pGame = new Game();
+  Game *pGame = new Game();
 
-//   return pGame;
-// };
+  return pGame;
+};
 
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(1280, 720), "The Grand Prix");
+
+  Game *game = gameInit(window);
 
   while (window.isOpen())
   {
